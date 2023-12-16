376a85efde3df0320baed0b14ac9d15192065b77
st43 <45909001+0si43@users.noreply.github.com>
2023-12-16 11:43:49 +0900

    made project

---
+++ M1Timer/M1Timer Watch App/M1TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  M1TimerApp.swift
+//  M1Timer Watch App
+//
+//  Created by Nakajima on 2023/12/16.
+//
+
+import SwiftUI
+
+@main
+struct M1Timer_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
