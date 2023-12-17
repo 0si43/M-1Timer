10010eaaca5aceeb06b4768ec6b0f93f1133ae5d
st43 <45909001+0si43@users.noreply.github.com>
2023-12-17 18:55:41 +0900

    added default button

--- M1Timer/M1Timer Watch App/Setting/SettingView.swift
+++ M1Timer/M1Timer Watch App/Setting/SettingView.swift
@@ -7,10 +7,11 @@
 
 import SwiftUI
 
+///  Back to Defaultの場合、pathとtimeIntervalがnilとなる
 struct TimeSetting: Identifiable {
     var id = UUID()
-    let path: Path
-    let timeInterval: TimeInterval
+    let path: Path?
+    let timeInterval: TimeInterval?
 }
 
 struct SettingView: View {
@@ -20,7 +21,8 @@ struct SettingView: View {
     private var timeSettings: [TimeSetting] {
         [
             TimeSetting(path: .timeLimit, timeInterval: timeLimit),
-            TimeSetting(path: .vibrationInterval, timeInterval: vibrationInterval)
+            TimeSetting(path: .vibrationInterval, timeInterval: vibrationInterval),
+            TimeSetting(path: nil, timeInterval: nil)
         ]
     }
 
@@ -28,30 +30,55 @@ struct SettingView: View {
         List {
             ForEach(timeSettings) { setting in
                 VStack(alignment: .leading) {
-                    Text(setting.path.title)
-                    .font(.footnote)
-                    Button {
-                        path.append(setting.path)
-                    } label: {
-                        Text(displayTime(setting))
+                    if let currentPath = setting.path,
+                       let timeInterval = setting.timeInterval {
+                        TimeIntervalButton(
+                            path: $path,
+                            currentPath: currentPath,
+                            timeInterval: timeInterval
+                        )
+                    } else {
+                        Button {
+                            timeLimit = 120
+                            vibrationInterval = 60
+                        } label: {
+                            Text(String(localized: "Back to Default", defaultValue: "Back to Default"))
+                        }
                     }
+                    
                 }
             }
         }
         .navigationTitle(String(localized: "Setting", defaultValue: "Setting"))
     }
+}
+
+private struct TimeIntervalButton: View {
+    @Binding var path: [Path]
+    let currentPath: Path
+    let timeInterval: TimeInterval
+    var body: some View {
+        Text(currentPath.title)
+        .font(.footnote)
+        Button {
+            path.append(currentPath)
+        } label: {
+            Text(displayTime(timeInterval))
+        }
+    }
     
-    private func displayTime(_ setting: TimeSetting) -> String {
+    private func displayTime(_ setting: TimeInterval) -> String {
         var displayTime = ""
-        displayTime += String(Int(setting.timeInterval) / 60)
+        displayTime += String(Int(timeInterval) / 60)
         displayTime += " " + String(localized: "min", defaultValue: "min") + " "
-        if Int(setting.timeInterval) % 60 != 0 {
-            displayTime += String((Int(setting.timeInterval) % 60))
+        if Int(timeInterval) % 60 != 0 {
+            displayTime += String((Int(timeInterval) % 60))
             displayTime  += " " + String(localized: "sec", defaultValue: "sec") + " "
         }
         return displayTime
     }
 }
+
 
 #Preview {
     @State var path = [Path.setting]
