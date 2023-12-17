db57e7a4f15be6680acf1b342fa4f68b91a7d0b8
st43 <45909001+0si43@users.noreply.github.com>
2023-12-17 13:25:57 +0900

    added an animation

--- M1Timer/M1Timer Watch App/TimerView.swift
+++ M1Timer/M1Timer Watch App/TimerView.swift
@@ -8,8 +8,31 @@
 import SwiftUI
 
 struct TimerView: View {
+    @State private var startAnimation = false
+    @State private var passMinutes: Int = 0
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            VStack {
+                Text(String(passMinutes))
+                    .font(.system(size: 80))
+                Text("min")
+                    .font(.caption)
+                    .bold()
+            }
+            .opacity(startAnimation ? 0.5 : 1)
+            .animation(.linear(duration: 1).repeatForever(autoreverses: false),
+                       value: startAnimation)
+            Circle()
+                .stroke(.cyan, style: .init(lineWidth: 4, lineCap: .round, lineJoin: .round, dash: [1, 13]))
+                .blur(radius: 1)
+                .rotationEffect(.degrees(startAnimation ? 360 : 0))
+                .animation(.linear(duration: 30).repeatForever(autoreverses: false),
+                           value: startAnimation)
+        }
+        .onAppear {
+            startAnimation = true
+        }
+        
     }
 }
 
