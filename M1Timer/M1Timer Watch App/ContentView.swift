17a9bb0523b8a6e879ff1f2267128aeb35ff2f50
0si43 <symn43@gmail.com>
2023-12-17 11:17:54 +0900

    added views

--- M1Timer/M1Timer Watch App/ContentView.swift
+++ M1Timer/M1Timer Watch App/ContentView.swift
@@ -8,32 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showTimerView = false
+    @State private var showSettingView = false
     var body: some View {
         NavigationStack {
             HStack {
                 VStack {
                     Button {
-                        // …
+                        showTimerView = true
                     } label: {
                         Image(systemName: "play")
                             .imageScale(.large)
                     }
                     .tint(.orange)
+                    .navigationDestination(isPresented: $showTimerView, destination: {
+                        TimerView()
+                    })
                     Text("Start")
                     .font(.footnote)
                 }
                 VStack {
                     Button {
-                        // …
+                        showSettingView = true
                     } label: {
                         Image(systemName: "gear")
                             .imageScale(.large)
                     }
+                    .navigationDestination(isPresented: $showSettingView, destination: {
+                        SettingView()
+                    })
                     Text("Setting")
                     .font(.footnote)
                 }
             }
-//            .padding()
             .navigationTitle("M-1 Timer")
         }
     }
