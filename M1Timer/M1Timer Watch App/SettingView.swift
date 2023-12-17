d03e964c0697ad10cccc4d6891ddb58ccf04d49b
st43 <45909001+0si43@users.noreply.github.com>
2023-12-17 11:17:54 +0900

    added views

---
+++ M1Timer/M1Timer Watch App/SettingView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingView.swift
+//  M1Timer Watch App
+//
+//  Created by Nakajima on 2023/12/17.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingView()
+}
---
+++ M1Timer/M1Timer Watch App/TimerView.swift
@@ -0,0 +1,18 @@
+//
+//  TimerView.swift
+//  M1Timer Watch App
+//
+//  Created by Nakajima on 2023/12/17.
+//
+
+import SwiftUI
+
+struct TimerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TimerView()
+}
