ff61334c38103efe9da28cf06b821c2e0db10449
0si43 <symn43@gmail.com>
2023-12-17 16:02:52 +0900

    made setting view

--- M1Timer/M1Timer Watch App/Setting/SettingView.swift
+++ M1Timer/M1Timer Watch App/Setting/SettingView.swift
@@ -8,8 +8,46 @@
 import SwiftUI
 
 struct SettingView: View {
+    @AppStorage("Timelimit") var timelimit: TimeInterval = 120
+    @AppStorage("VibrationInterval") var vibrationInterval: TimeInterval = 60
+    @State var selectedTimelimit: Timelimit = .two
+    @State var selectedVibrationInterval: TimeInterval = 60
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack() {
+                Text("Time\nlimit")
+                Picker("", selection: $selectedTimelimit) {
+                    ForEach(Timelimit.allCases) {
+                        Text($0.title)
+                    }
+                }
+                .onChange(of: selectedTimelimit) {
+                    if let interval = selectedTimelimit.interval {
+                        vibrationInterval = interval
+                    } else {
+                        // …
+                    }
+                }
+                Text("min")
+            }
+            HStack() {
+                Text("Interval")
+                Picker("", selection: $selectedVibrationInterval) {
+                    ForEach(1..<61) {
+                        Text(String($0))
+                    }
+                }
+                .onChange(of: vibrationInterval) {
+                    vibrationInterval = selectedVibrationInterval
+                }
+                Text("sec")
+            }
+        }
+        .onAppear {
+            selectedTimelimit = Timelimit(interval: timelimit)
+            selectedVibrationInterval = vibrationInterval
+        }
     }
 }
 
