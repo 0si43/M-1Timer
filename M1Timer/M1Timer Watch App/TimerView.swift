c69b942f59f461fe559cc7fb26fc56054ca2891b
st43 <45909001+0si43@users.noreply.github.com>
2023-12-17 13:41:49 +0900

    limit time

--- M1Timer/M1Timer Watch App/TimerView.swift
+++ M1Timer/M1Timer Watch App/TimerView.swift
@@ -8,20 +8,24 @@
 import SwiftUI
 
 struct TimerView: View {
-    @State private var startAnimation = false
-    @State private var passMinutes: Int = 0
+    @State var startAnimation = false
+    @State var passMinutes: Int = 0
+    @State var totalTime: TimeInterval = 120
+    @State var timer: Timer?
+    @State var interval: TimeInterval = 60
     var body: some View {
         ZStack {
             VStack {
                 Text(String(passMinutes))
                     .font(.system(size: 80))
+                    .opacity(startAnimation ? 0.5 : 1)
+                    .animation(.linear(duration: 1).repeatForever(autoreverses: false),
+                               value: startAnimation)
                 Text("min")
+                    .foregroundStyle(.gray)
                     .font(.caption)
                     .bold()
             }
-            .opacity(startAnimation ? 0.5 : 1)
-            .animation(.linear(duration: 1).repeatForever(autoreverses: false),
-                       value: startAnimation)
             Circle()
                 .stroke(.cyan, style: .init(lineWidth: 4, lineCap: .round, lineJoin: .round, dash: [1, 13]))
                 .blur(radius: 1)
@@ -31,8 +35,11 @@ struct TimerView: View {
         }
         .onAppear {
             startAnimation = true
+            timer = Timer.scheduledTimer(withTimeInterval: interval, repeats: true) { timer in
+                passMinutes += 1
+            }
         }
-        
+        .navigationTitle("\(Int(totalTime / 60)) min")
     }
 }
 
